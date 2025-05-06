872e8a9f4b3da716737eeb91391e7fead9dca41a
Mushak Shareef <sivcanopi@gmail.com>
2025-05-06 12:01:41 +0530

    01Jun21

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let inPutNameJs   = document.getElementById("input-HTML");
+let selectJs      = document.getElementById("select-HTML");
+let imgJs         = document.getElementById("posterImg-HTML");
+
+
+
+
+
+
+
+
+
+
+
+
